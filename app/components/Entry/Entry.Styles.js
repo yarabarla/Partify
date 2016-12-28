b074d9af8def427ae5e9797c5cd567068569cd7c
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2016-12-28 01:22:11 -0600

    Splash and login layout done

--- app/components/Entry/Entry.Styles.js
+++ app/components/Entry/Entry.Styles.js
@@ -1,22 +1,35 @@
 import { StyleSheet } from 'react-native';
 
+const backgroundColor = '#e8eff9'
+
 const styles = StyleSheet.create({
-  container: {
+  entryContainer: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'lightskyblue'
+    backgroundColor: backgroundColor
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  loginContainer: {
+    flex: 1.5,
+    alignSelf: 'stretch',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: backgroundColor
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+  loginText: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 30
+  }
 });
+
+styles.loginButton = (color) => ({
+    height: 90,
+    backgroundColor: color,
+    justifyContent: 'space-around'
+})
 
 export default styles;
--- app/components/Entry/Entry.js
+++ app/components/Entry/Entry.js
@@ -1,43 +1,64 @@
 import React, { Component } from 'react';
 import {
   Text,
-  TextInput,
-  Button,
+  Image,
+  TouchableHighlight,
   View
 } from 'react-native';
 
 import styles from './Entry.Styles';
 
+const LoginContainer = () => (
+  <View style={styles.loginContainer}>
+    <TouchableHighlight onPress={() => {}} underlayColor='#1f3563' style={styles.loginButton('#3b5998')}>
+      <Text style={styles.loginText}>Facebook Login</Text>
+    </TouchableHighlight>
+    <TouchableHighlight onPress={() => {}} underlayColor='#e89600' style={styles.loginButton('orange')}>
+      <Text style={styles.loginText}>Enter As Guest</Text>
+    </TouchableHighlight>
+  </View>
+)
+
 export default class Entry extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super()
+    this.state = {
+      timePassed: false
+    }
+  }
+
+  setTimePassed() {
+    this.setState({
+      timePassed: true
+    })
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setTimePassed()
+    }, 1000)
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style = {{width: 200, textAlign: 'center', flex: 2}}
-          placeholder = "Holding this shit"
-          onChangeText = {(text) => this.value = text}
-        />
-        <Button
-          onPress = {() => alert(this.value)}
-          style = {{flex:2}}
-          title = 'Join Room'
-          color = 'purple'
-        />
-  {/*      <Text style={styles.welcome}>
-          Go fuck yourself!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, suck a cock.
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>*/}
-      </View>
-    )
+    if (!this.state.timePassed) {
+      return (
+        <View style={styles.entryContainer}>
+          <Image
+            source={require('../../assets/logo.png')}
+            style={{flex: 1, resizeMode: 'contain'}}
+          />
+        </View>
+      )
+    } else { // Replace with a more elegant scheme
+      return (
+        <View style={styles.entryContainer}>
+          <Image
+            source={require('../../assets/logo.png')}
+            style={{flex: 1, resizeMode: 'contain'}}
+          />
+          <LoginContainer />
+        </View>
+      )
+    }
   }
 }
